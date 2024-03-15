e16a89d7aa126ce7fca8a842e006dfdd07664c03
fejiro <fejiropaul95@gmail.com>
2024-03-15 07:32:00 +0000

    class based views

--- store/urls.py
+++ store/urls.py
@@ -3,8 +3,11 @@ from . import views
 
 # URLConf
 urlpatterns = [
-    path('products/', views.product_list),
-    path('products/<int:id>/', views.product_detail),
+    # path('products/', views.product_list),
+    # moving the view function below from the function based view above to the class based view below
+    # the class based view has a method as_view() which converts the class to a normal function based view
+    path('products/', views.ProductList.as_view()),
+    path('products/<int:id>/', views.ProductDetail.as_view()),
     path('collections/', views.collection_list),
     path('collections/<int:pk>/', views.collection_detail, name='collection-detail'),
 ]
--- store/views.py
+++ store/views.py
@@ -3,42 +3,50 @@ from django.shortcuts import get_object_or_404
 from django.http import HttpResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.views import APIView
 from rest_framework import status
 from .models import Collection, Product
 from .serializers import CollectionSerializer, ProductSerializer
 
+# up till now we have been using function based views
+# django also support class based view which make our code cleaner and concise
+# they also provide reuse opportunities
 
-@api_view(['GET', 'POST'])
-def product_list(request):
-    if request.method == 'GET':
+# to convert the product_list view to a class based view we use the apiview class from rest framework
+# it is the base class for all class based views
+class ProductList(APIView):
+    # we define two methods, get for handling get requests and post for post requests
+    # incoming requests automatically get dispatched to either of these based on the method
+    # we no longer have the if statements in our code
+    def get(self,request):
         queryset = Product.objects.select_related('collection').all()
         serializer = ProductSerializer(
             queryset, many=True, context={'request': request})
         return Response(serializer.data)
-    elif request.method == 'POST':
+    def post(self,request):
         serializer = ProductSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-
-@api_view(['GET', 'PUT', 'DELETE'])
-def product_detail(request, id):
-    product = get_object_or_404(Product, pk=id)
-    if request.method == 'GET':
+class ProductDetail(APIView):
+    def get(self,request, id):
+        product = get_object_or_404(Product, pk=id)
         serializer = ProductSerializer(product)
         return Response(serializer.data)
-    elif request.method == 'PUT':
+    def put(self,request,id):
+        product = get_object_or_404(Product, pk=id)
         serializer = ProductSerializer(product, data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
-    elif request.method == 'DELETE':
+
+    def delete(self,request,id):
+        product = get_object_or_404(Product, pk=id)
         if product.orderitems.count() > 0:
             return Response({'error': 'Product cannot be deleted because it is associated with an order item.'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
         product.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
-
 
 @api_view(['GET', 'POST'])
 def collection_list(request):
@@ -71,3 +79,4 @@ def collection_detail(request, pk):
             return Response({'error': 'Collection cannot be deleted because it includes one or more products.'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
         collection.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
